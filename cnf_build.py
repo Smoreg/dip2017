11ff6e300f5d3e48049dae7d6bf348b586a67d04
Smoreg <semenchenkoke@gmail.com>
2017-01-31 11:38:57 +0300

    grouper

--- cnf_build.py
+++ cnf_build.py
@@ -1,6 +1,93 @@
 import numpy as np
+DEBUG = True
 
 class CNF:
     pass
 
-<class 'list'>: [PL 128 11111011000001001111011001110101001111111101011001000111101010101110100001000101000101101100100101001111010110001110110101000000, PL 128 11101011001010000100110011110110011110111100001101011101101011010101100111111001001000100110001101011100000110010001110010011111, PL 128 00011001101010011110011000110010010110010100100101000111100111101110010101000011111111010001111000000111110110111000111101000000, PL 128 11110111010100011100001101010101000010011001001101110011101000010001101011000000001000010000000101000000000010111000010010100000, PL 128 00000000011101110001010100011110001010100110011110101010011011010001011110010100000111011001000101001110110110010100010001111010, PL 128 01111110001111010011001010011001001011111110111010101100100011001010001110011110110001011001110110010001011110010000000110001110, PL 128 00000000001100100111000100001111100110011100100001010111011111111011010110100110011001000110101110010010111001111000100111000100, PL 128 11110001100101101010110110010101110110010001000010011001011100010010001111100011101111101101000100011110011001010010011110011100, PL 128 10101000010110000011100111010010001011101110000000010011110010101111001001111011010001110011001110111100000000010100010010100100, PL 128 01001010111111011000001000000000111101110010100001011001001111010010101101101110011110000100000001111110101011111111110111110000]
+
+
+def grouper(summands, th):
+    # summands unique
+    # sorted !
+    th = th
+    groups = np.arange(len(summands))
+
+    # Merge all
+    summands = np.copy(summands)
+    active_flags = np.ones(len(summands), dtype=np.bool)
+
+    for num, _ in enumerate(summands):
+        if active_flags[num]:
+            tmp_gr = np.where((groups != groups[num]))[0]
+            tmp_gr = tmp_gr[tmp_gr > num]
+            for num2 in tmp_gr:
+                if np.all(np.in1d(summands[num2][summands[num2] > 0], summands[num])):
+                    active_flags[num2] = False
+                    groups[num2] = groups[num]
+
+    # Choose union
+
+    active_indexs = np.arange(active_flags.shape[0])[active_flags == True]
+
+    for ind_main in active_indexs:
+        if ind_main in np.arange(active_flags.shape[0])[active_flags == True]:
+            merge_score = np.zeros(len(summands)) - 1
+            main = np.unique(summands[ind_main])
+            if len(main[main > 0]) >= th:
+                continue
+            for ind_part in active_indexs:
+                if ind_main != ind_part:
+                    part = np.unique(summands[ind_part])
+                    main = main[main > 0]
+                    part = part[part > 0]
+                    unite = np.unique(np.union1d(main, part))
+                    uniq_len = len(unite[unite > 0]) # free space
+                    if uniq_len > th:
+                        merge_score[ind_part] = -1
+                        continue
+                    new_len = len(unite[unite > 0]) - len(main[main > 0])
+                    copylen = len(part[part > 0]) - new_len
+                    if DEBUG and not new_len:
+                        raise Exception('zero new len {} {}'.format(summands[ind_main], summands[ind_part]))
+                    merge_score[ind_part] = copylen / new_len
+
+            if max(merge_score) >= 0:
+                m_arg = np.argmax(merge_score)
+                tmp_summand = np.zeros(summands.shape[1],dtype=summands.dtype)
+                tmp_summand_vars = np.unique(np.union1d(summands[ind_main], summands[m_arg]))
+                tmp_summand[:len(tmp_summand_vars)] = tmp_summand_vars
+                summands[ind_main] = tmp_summand
+                groups[groups == groups[m_arg]] = groups[ind_main]
+
+                active_flags[m_arg] = False
+
+                # Merge new
+                for num in (np.where([groups != groups[ind_main]][0] & active_flags))[0]:
+                    if np.all(np.in1d(summands[num][summands[num] > 0], summands[ind_main])):
+                        active_flags[num] = False
+                        groups[num] = groups[ind_main]
+
+    #getgroups
+    gr = np.unique(groups)
+    a = dict()
+    for i in gr:
+        a[i] = np.unique(summands[groups == i])
+    return groups, a
+
+
+if __name__ == '__main__':
+    s = np.array(
+        [
+            [3, 4, 5],
+            [4, 6, 7],
+            [4, 5, 0],
+            [2, 5, 0],
+            [6, 0, 0],
+            [2, 0, 0]
+        ])
+    s = np.c_[s, np.zeros_like(s), np.zeros_like(s), np.zeros_like(s)]
+    for num in range(3,6):
+        print(num)
+        res, a = grouper(s,num)
+        print(np.c_[s, res])
+        print(a)
